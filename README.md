2e4f129d5716833fa34514c3dd53ff3a81355416
jakobsaadbye <31799001+jakobsaadbye@users.noreply.github.com>
2024-12-11 13:49:57 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,18 @@
+# Quick start
+
+Install Deno:
+https://docs.deno.com/runtime/getting_started/installation/
+
+macOS:
+```console
+curl -fsSL https://deno.land/install.sh | sh
+```
+Windows:
+```console
+irm https://deno.land/install.ps1 | iex
+```
+
+Start the server:
+```
+deno task start
+```
---
+++ tables.sql
@@ -0,0 +1,43 @@
+create table if not exists boards(
+    id varchar(64) primary key,
+    title varchar(255),
+    created_at timestamptz default current_timestamp,
+    updated_at timestamptz default current_timestamp
+);
+
+create table if not exists columns(
+    id varchar(64) primary key,
+    board_id varchar(64) references boards(id) on delete cascade,
+    title varchar(255),
+    position real
+);
+
+create table if not exists todos(
+    id varchar(64) primary key,
+    board_id varchar(64) references boards(id) on delete cascade,
+    column_id varchar(64) references columns(id) on delete cascade,
+    title varchar(255),
+    description varchar,
+    position real,
+    updated_at timestamptz default current_timestamp
+);
+
+create table if not exists crr_changes(
+    id text not null,
+    type text not null,
+    tbl_name text not null,
+    col_id text,
+    row_id text not null,
+    value any,
+    site_id text not null,
+    created_at bigint not null,
+    applied_at bigint not null,
+    seq integer,
+    primary key(id, type, tbl_name, col_id, row_id, value, site_id, created_at, applied_at, seq)
+);
+
+create table if not exists crr_client(
+    site_id primary key,
+    last_pulled_at bigint not null default 0,
+    last_pushed_at bigint not null default 0
+);
